77028273d890502df57a038faa6c3f895152269b
Vitor <vitor.vfm@gmail.com>
2015-05-26 10:58:27 -0600

    Moved packages installation to its won directory
    Also added a configuration module for the env dictionary in all deployments

--- network_deployment/fabfile.py
+++ network_deployment/fabfile.py
@@ -6,50 +6,12 @@ from fabric.colors import green, red
 from fabric.contrib.files import append
 import string
 
+from env_config import roledefs, read_dict
+
 
 ############################ Config ########################################
 
-# Read the values from a node file into a list
-@hosts('localhost')
-def read_nodes(node):
-    node_info = open(node, 'r')
-    node_string = node_info.read().splitlines()
-    # remove comments (lines that have # in the beginning)
-    # node_string_stripped = [node_element.strip() for node_element in node_string if node_element[0] != '#']
-    node_info.close()
-    #print node_string_stripped
-    return node_string
-
-# Make a dictionary from a config file with the format "KEY=value" on each 
-# line
-@hosts('localhost')
-def read_dict(config_file):
-    config_file_info = open(config_file, 'r')
-    config_file_without_comments = [line for line in config_file_info.readlines() if line[0] != '#']
-    config_file_string = "".join(config_file_without_comments)
-    # config_file_string = config_file_info.read().replace('=','\n').splitlines()
-    config_file_string = config_file_string.replace('=','\n').splitlines()
-    config_file_string_stripped = [config_element.strip() for config_element in config_file_string]
-    config_file_dict = dict()
-    
-    # Make a dictionary from the string from the file with the the first value
-    # on a line being the key to the value after the '=' on the same line
-    for config_file_key_index in range(0,len(config_file_string_stripped)-1,2):
-        config_file_value_index = config_file_key_index + 1
-        config_file_dict[config_file_string_stripped[config_file_key_index]] = config_file_string_stripped[config_file_value_index]
-    
-    config_file_info.close()
-
-    #run("rm -rf %s" % config_file)
-    return config_file_dict
-
-
-# Get nodes and their roles from the config files
-compute_nodes = read_nodes('config_files/compute_nodes')
-controller_nodes = read_nodes('config_files/controller_nodes')
-network_nodes = read_nodes('config_files/network_nodes')
-#env.hosts = compute_nodes + controller_nodes + network_nodes
-env.roledefs = { 'controller':controller_nodes, 'compute':compute_nodes, 'network':network_nodes }
+env.roledefs = roledefs
 
 # Get configuration dictionaries from the config files
 compute_tunnels = read_dict('config_files/compute_instance_tunnels_interface_config')
---
+++ packages_installation/fabfile.py
@@ -0,0 +1,93 @@
+from __future__ import with_statement
+from fabric.api import *
+from fabric.decorators import with_settings
+from fabric.context_managers import cd
+from fabric.colors import green, red
+import string
+
+import env_config
+
+
+############################ Config ########################################
+
+env.roledefs = env_config.roledefs
+
+# config files for MariaDB
+mariadb_repo = 'config_files/mariadb_repo'
+mariadb_mysqld_specs = 'config_files/mariadb_mysqld_specs'
+
+################### Deployment ########################################
+
+# General function to install packages that should be in all or several nodes
+@with_settings(warn_only=True)
+def install_packages():
+    
+    # Install NTP
+    sudo('yum -y install ntp')
+    # Are we using an NTP server?
+    # enable NTP
+    #sudo('systemctl enable ntpd.service')
+    #sudo('systemctl start ntpd.service')
+
+    # Install EPEL (Extra Packages for Entreprise Linux
+    sudo('yum -y install yum-plugin-priorities')
+    sudo('yum -y install epel-release')
+
+    # Install RDO repository for Juno
+    sudo('yum -y install http://rdo.fedorapeople.org/openstack-juno/rdo-release-juno.rpm')
+
+    # Install Crudini
+    sudo("yum -y install crudini")
+
+
+    # Install MariaDB
+    # Only on controller node(s)
+    
+    if env.host_string in env.roledefs['controller']:
+	    # get packages
+        sudo('yum -y install mariadb mariadb-server MySQL-python')
+
+        # set the config file
+        section_header = '\[mysqld\]'
+
+        with cd('/etc/'):
+            # check if the section is already in the file
+            if run("grep '{}' my.cnf".format(section_header)).return_code == 0:
+                # do a search and replace for all the variables
+                config_file = open(mariadb_mysqld_specs, 'r').readlines(True)
+                # make a backup
+                sudo("cp my.cnf my.cnf.back12")
+
+                for line in config_file:
+                    # delete old line in the file
+                    # remove \n
+                    line = line[:-1]
+                    if ('=' in line):
+                        pattern_to_find = line[:line.index('=')]
+                    else:
+                        pattern_to_find = line
+                    sudo('sed -i "/{}/ d" my.cnf'.format(pattern_to_find))
+                    # append new line with the new value under the header
+                    sudo('''sed -i '/{}/ a\{}' my.cnf'''.format(section_header,line))
+
+            else:
+                # simply add the section
+                config_file = '\n' + section_header + '\n'
+                config_file += open(mariadb_mysqld_specs, 'r').read()
+                sudo('echo -e "{}" >>my.cnf'.format(config_file))
+
+        # enable MariaDB
+        sudo('systemctl enable mariadb.service')
+        sudo('systemctl start mariadb.service')
+        
+
+    # Upgrade to implement changes
+    sudo('yum -y upgrade')
+
+def ask_for_reboot():
+    sudo('wall Everybody please reboot')
+
+@roles('controller','compute','network')
+def deploy():
+    execute(install_packages)
+    execute(ask_for_reboot)
