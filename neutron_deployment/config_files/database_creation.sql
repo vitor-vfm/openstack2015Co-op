8805a9803531cf27bdc7e8dec1b523b604ea6bdc
Vitor <vitor.vfm@gmail.com>
2015-05-28 08:40:53 -0600

    Removed old unused directory

--- neutron_deployment/config_files/database_creation.sql
+++
@@ -1,8 +0,0 @@
-# this script creates a database to be used by Neutron
-# 'NEUTRON_DBPASS' should be replaced by a suitable password
-
-CREATE DATABASE neutron;
-GRANT ALL PRIVILEGES ON neutron.* TO 'neutron'@'localhost' \
-  IDENTIFIED BY 'NEUTRON_DBPASS';
-GRANT ALL PRIVILEGES ON neutron.* TO 'neutron'@'%' \
-  IDENTIFIED BY 'NEUTRON_DBPASS';
