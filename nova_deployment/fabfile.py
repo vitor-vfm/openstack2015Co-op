8d6e124dc4ab0ff71a09a76bcbdbd07360485c22
Vitor <vitor.vfm@gmail.com>
2015-05-28 09:31:50 -0600

    Small fixes to nova deployment

--- nova_deployment/fabfile.py
+++ nova_deployment/fabfile.py
@@ -7,11 +7,17 @@ from fabric.contrib.files import append
 import logging
 import string
 
+import sys
+sys.path.append('../global_config_files')
+import env_config
+
 
 
 logging.basicConfig(filename='/tmp/juno2015.log',level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 ############################ Config ########################################
+
+env.roledefs = env_config.roledefs
 
 nova_config_file = 'nova_config'
 
@@ -196,7 +202,7 @@ def upload_files_on_compute():
     put(global_config_file_location)
     
 
-#@roles('compute')
+@roles('compute')
 def setup_nova_on_compute():
     download_packages()
 
@@ -209,12 +215,11 @@ def setup_nova_on_compute():
     RABBIT_PASS = get_parameter(global_config_file_name, 'rabbitmq', 'RABBIT_PASS')
     NETWORK_MANAGEMENT_IP = get_parameter(compute_management_interface_file_name, "''", 'IPADDR')
 
-#    setup_nova_config_files_on_compute(NOVA_PASS, NOVA_DBPASS, RABBIT_PASS, NETWORK_MANAGEMENT_IP)        
-    #hardware_accel_check()
+    setup_nova_config_files_on_compute(NOVA_PASS, NOVA_DBPASS, RABBIT_PASS, NETWORK_MANAGEMENT_IP)        
     start_services_on_compute()
     
 
-#@roles('controller')   
+@roles('controller')   
 def setup_nova_on_controller():
     
     host_command = 'sudo_log -- sh -c "{}"'.format("echo '{}' >> /etc/hosts".format("{}        controller".format(env.host))) 
@@ -248,24 +253,17 @@ def setup_nova_on_controller():
     start_nova_services_on_controller()
         
 
-
-
-
-
-
-
-
-
 ################### Deployment ########################################
 
 def deploy():
-    setup_nova_on_controller()
-    setup_nova_on_compute()
+    execute(setup_nova_on_controller)
+    execute(setup_nova_on_compute)
 
 ######################################## TDD #########################################
 
 
 
+@roles('controller')
 def verify():
     source_command = "source admin-openrc.sh"
     with prefix(source_command):
@@ -276,6 +274,6 @@ def verify():
 def tdd():
     with settings(warn_only=True):
         # to be done on the controller node
-        verify()
+        execute(verify)
 
 
