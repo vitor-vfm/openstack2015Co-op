6c4352dc8cfee321681188e41d4a89ae658fb1a4
Umair Hussain <suhussai@ualberta.ca>
2015-06-04 08:22:07 -0600

    Added reminder to run packages installation before network deployment. Added yum install ntp to network deployment to ensure we have ntp when configuring

--- 1-network_deployment/fabfile.py
+++ 1-network_deployment/fabfile.py
@@ -135,6 +135,10 @@ def configureNTP_on_controller():
     NTP_SERVERS_HOSTNAME = ["ntp.amnic.net","ntp1.torix.ca","ntp2.torix.ca"]
     confFile = '/etc/ntp.conf'
 
+
+    logging.debug("Making sure we have ntp")
+    sudo('yum -y install ntp')
+
     # check if file has been configured already
     if sudo("grep '{}' {}".format(newLine,confFile), warn_only=True).return_code == 0:
         message = 'NTP conf file has already been set. Nothing done'
@@ -218,6 +222,8 @@ def deploy():
     # create log dictionary (to set up the log formatting)
     log_dict = {'host_string':'','role':''}
     logging.debug('Starting deployment',extra=log_dict)
+   
+    local("Ensure that you've run packages installation fabfile first")
 
     with settings(warn_only=True):
         execute(controller_network_deploy)
