67269ca1437380b963acdee65f4bba2d905e8ddb
Umair Hussain <suhussai@ualberta.ca>
2015-06-08 13:16:54 -0600

    removed config_files dir from neutron deployment

--- 6-neutron_deployment/config_files/database_creation.sql
+++
@@ -1,7 +0,0 @@
--- this script creates a database to be used by Neutron
--- 'NEUTRON_DBPASS' should be replaced by a suitable password
-CREATE DATABASE IF NOT EXISTS neutron;
-GRANT ALL PRIVILEGES ON neutron.* TO 'neutron'@'localhost' \
-  IDENTIFIED BY 'NEUTRON_DBPASS';
-GRANT ALL PRIVILEGES ON neutron.* TO 'neutron'@'%' \
-  IDENTIFIED BY 'NEUTRON_DBPASS';
