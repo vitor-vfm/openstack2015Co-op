6d3b3a30ed2ea2a19e454d933b9f615d71c4a542
Vitor <vitor.vfm@gmail.com>
2015-06-09 15:26:04 -0600

    Added the 'runCheck' wrapper function to myLib'

--- myLib.py
+++ myLib.py
@@ -1,4 +1,5 @@
 from fabric.colors import green, red
+from fabric.api import run
 
 
 def printMessage(status, msg):
@@ -13,3 +14,16 @@ logging.basicConfig(filename='/tmp/test.log',level=logging.DEBUG)
 #logging.debug('This message should go to the log file')
 #logging.info('So should this')
 #logging.warning('And this, too')
+
+
+def runCheck(msg,command):
+
+    out = run(command,warn_only=True)
+    if out.return_code == 0:
+        result = 'good'
+        logging.debug('Success on: ' + msg)
+    else:
+        result = 'oops'
+        errormsg = 'Failure on: ' + msg
+        logging.error(errormsg)
+    printMessage(result,msg)
