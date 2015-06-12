a58b513da458c1ab70d09ae3779004474ad10789
Umair Hussain <suhussai@ualberta.ca>
2015-06-12 13:04:50 -0600

    Cleaning up fabfile for nova. Finished nova tdd for nova service list (UNTESTED). Need to do tdd for nova image list.

--- 5-nova_deployment/fabfile.py
+++ 5-nova_deployment/fabfile.py
@@ -10,8 +10,7 @@ import string
 import sys
 sys.path.append('../global_config_files')
 import env_config
-from env_config import log_debug, log_info, log_error, run_log, sudo_log
-
+from myLib import align_n, align_y, database_check, keystone_check, run_v
 
 
 ############################ Config ########################################
@@ -19,27 +18,13 @@ from env_config import log_debug, log_info, log_error, run_log, sudo_log
 env.roledefs = env_config.roledefs
 passwd = env_config.passwd
 
-admin_openrc = "../global_config_files/admin-openrc.sh"
-
-demo_openrc = "../global_config_files/demo-openrc.sh"
-
 etc_nova_config_file = "/etc/nova/nova.conf"
-
-# logging setup
-
-log_file = 'nova_deployment.log'
-env_config.setupLoggingInFabfile(log_file)
     
 ################### General functions ########################################
 
-def get_parameter(config_file, section, parameter):
-    crudini_command = "crudini --get {} {} {}".format(config_file, section, parameter)
-    return local(crudini_command, capture=True)
-#    return sudo_log(crudini_command)
-
 def set_parameter(config_file, section, parameter, value):
     crudini_command = "crudini --set {} {} {} {}".format(config_file, section, parameter, value)
-    sudo_log(crudini_command)
+    run(crudini_command)
 
 
 def setup_nova_database_on_controller(NOVA_DBPASS):
@@ -49,7 +34,7 @@ def setup_nova_database_on_controller(NOVA_DBPASS):
 
     
     print("mysql commands are: " + mysql_commands)
-    sudo_log('echo "{}" | mysql -u root'.format(mysql_commands))
+    run('echo "{}" | mysql -u root'.format(mysql_commands))
     
 
 
@@ -58,24 +43,24 @@ def setup_nova_keystone_on_controller(NOVA_PASS):
     with prefix(source_command):
 
         if 'nova' not in sudo("keystone user-list"):
-            sudo_log("keystone user-create --name nova --pass {}".format(NOVA_PASS))
-            sudo_log("keystone user-role-add --user nova --tenant service --role admin")
+            run("keystone user-create --name nova --pass {}".format(NOVA_PASS))
+            run("keystone user-role-add --user nova --tenant service --role admin")
         else:
             log_debug('User nova already in user list')
 
         if 'nova' not in sudo("keystone service-list"):
-            sudo_log("keystone service-create --name nova --type compute --description 'OpenStack Compute'")
+            run("keystone service-create --name nova --type compute --description 'OpenStack Compute'")
         else:
             log_debug('Service nova already in service list')
 
         if '8774' not in sudo("keystone endpoint-list"):
-            sudo_log("keystone endpoint-create --service-id $(keystone service-list | awk '/ compute / {print $2}') --publicurl http://controller:8774/v2/%\(tenant_id\)s  --internalurl http://controller:8774/v2/%\(tenant_id\)s --adminurl http://controller:8774/v2/%\(tenant_id\)s --region regionOne")
+            run("keystone endpoint-create --service-id $(keystone service-list | awk '/ compute / {print $2}') --publicurl http://controller:8774/v2/%\(tenant_id\)s  --internalurl http://controller:8774/v2/%\(tenant_id\)s --adminurl http://controller:8774/v2/%\(tenant_id\)s --region regionOne")
         else:
             log_debug('Endpoint 8774 already in endpoint list')
     
 def setup_nova_config_files_on_controller(NOVA_PASS, NOVA_DBPASS, RABBIT_PASS, CONTROLLER_MANAGEMENT_IP):
     installation_command = "yum install -y openstack-nova-api openstack-nova-cert openstack-nova-conductor openstack-nova-console openstack-nova-novncproxy openstack-nova-scheduler python-novaclient"
-    sudo_log(installation_command)
+    run(installation_command)
     
     set_parameter(etc_nova_config_file, 'database', 'connection', 'mysql://nova:{}@controller/nova'.format(NOVA_DBPASS))
 
@@ -108,19 +93,15 @@ def setup_nova_config_files_on_controller(NOVA_PASS, NOVA_DBPASS, RABBIT_PASS, C
 
 
 def populate_database_on_controller():
-    sudo_log("su -s /bin/sh -c 'nova-manage db sync' nova")
+    run("su -s /bin/sh -c 'nova-manage db sync' nova")
 
 def start_nova_services_on_controller():
     enable_all = "systemctl enable openstack-nova-api.service openstack-nova-cert.service openstack-nova-consoleauth.service openstack-nova-scheduler.service openstack-nova-conductor.service openstack-nova-novncproxy.service"
 
     start_all = "systemctl start openstack-nova-api.service openstack-nova-cert.service openstack-nova-consoleauth.service openstack-nova-scheduler.service openstack-nova-conductor.service openstack-nova-novncproxy.service"
     
-    sudo_log(enable_all)
-    sudo_log(start_all)
-
-def download_packages():
-    # make sure we have crudini
-    sudo_log('yum install -y crudini')
+    run(enable_all)
+    run(start_all)
 
 
 
@@ -128,7 +109,7 @@ def setup_nova_config_files_on_compute(NOVA_PASS, NOVA_DBPASS, RABBIT_PASS, NETW
 
 
 
-    sudo_log('yum install -y openstack-nova-compute sysfsutils')
+    run('yum install -y openstack-nova-compute sysfsutils')
     
     set_parameter(etc_nova_config_file, 'DEFAULT', 'rpc_backend', 'rabbit')
     set_parameter(etc_nova_config_file, 'DEFAULT', 'rabbit_host', 'controller')
@@ -161,28 +142,20 @@ def setup_nova_config_files_on_compute(NOVA_PASS, NOVA_DBPASS, RABBIT_PASS, NETW
 
 def hardware_accel_check():
     with settings(warn_only=True):
-        output = sudo_log("egrep -c '(vmx|svm)' /proc/cpuinfo")    
+        output = run("egrep -c '(vmx|svm)' /proc/cpuinfo")    
 
     if int(output) < 1:
         # we need to do more configuration
         set_parameter(etc_nova_config_file, 'libvirt', 'virt_type', 'qemu')
 
 def start_services_on_compute():
-    sudo_log("systemctl enable libvirtd.service openstack-nova-compute.service")
-    sudo_log("systemctl start libvirtd.service openstack-nova-compute.service")
+    run("systemctl enable libvirtd.service openstack-nova-compute.service")
+    run("systemctl start libvirtd.service openstack-nova-compute.service")
 
 
 @roles('compute')
 def setup_nova_on_compute():
 
-    download_packages()
-    put(admin_openrc)
-
-    # variable setup
-
-    # NOVA_DBPASS = get_parameter(env_config.global_config_file,'mysql','NOVA_DBPASS')
-    # NOVA_PASS = get_parameter(env_config.global_config_file,'keystone','NOVA_PASS')
-    # RABBIT_PASS = get_parameter(env_config.global_config_file,'rabbitmq', 'RABBIT_PASS')
     NETWORK_MANAGEMENT_IP = env_config.networkManagement['IPADDR']
 
     setup_nova_config_files_on_compute(passwd['NOVA_PASS'], passwd['NOVA_DBPASS'], passwd['RABBIT_PASS'], NETWORK_MANAGEMENT_IP)        
@@ -192,25 +165,6 @@ def setup_nova_on_compute():
 @roles('controller')   
 def setup_nova_on_controller():
 
-    host_command = 'sudo_log -- sh -c "{}"'.format("echo '{}' >> /etc/hosts".format("{}        controller".format(env.host))) 
-    #    sudo_log(host_command)
-    
-    
-    # fixing bind-address on /etc/my.cnf
-    
-    # bindCommand = "sed -i.bak 's/^\(bind-address=\).*/\1 {} /' /etc/my.cnf".format(env.host)
-    bindCommand = "sed -i '/bind-address/s/=.*/={}/' /etc/my.cnf".format(env.host)
-    #    sudo_log(bindCommand)
-    
-    #    sudo_log("systemctl restart mariadb")
-    
-    download_packages()
-    put(admin_openrc)
-    
-    # variable setup
-    # NOVA_DBPASS = get_parameter(env_config.global_config_file,'mysql','NOVA_DBPASS')
-    # NOVA_PASS = get_parameter(env_config.global_config_file,'keystone','NOVA_PASS')
-    # RABBIT_PASS = get_parameter(env_config.global_config_file,'rabbitmq', 'RABBIT_PASS')
     CONTROLLER_MANAGEMENT_IP = env_config.controllerManagement['IPADDR']
 
     # setup nova database
@@ -234,13 +188,65 @@ def deploy():
 @roles('controller')
 def verify():
 
-    env_config.database_check('nova')
-    env_config.keystone_check('nova')
+    database_check('nova')
+    keystone_check('nova')
+    
+    nova_services = ['nova-conductor','nova-consoleauth','nova-scheduler', 'nova-cert']
+    
+    with prefix(env_config.admin_openrc):
+        for service in nova_services:
+            if service in run("nova service-list"):
+                print align_y("{} exists in nova service list".format(service)) 
+                check_for = {'6':'controller','8':'internal','10':'enabled','12':'up'}
 
-    source_command = "source admin-openrc.sh"
-    with prefix(source_command):
-        sudo_log("nova service-list")
-        sudo_log("nova image-list")
+                for location, correct_value in check_for.iteritems():
+                    if (run("nova service-list | awk '/{}/ {print $"+ location +"}'".format(service)) == correct_value):
+                        print align_y("{} host is {}".format(service, correct_value)) 
+                    else:
+                        print align_n("{} host is NOT {}".format(service, correct_value))  
+            else:
+                print align_n("{} does NOT exist in nova service list".format(service)) 
+
+        # separate check for nova-compuet as it has different values
+        service = 'nova-compute'
+        if service in run("nova service-list"):
+            print align_y("{} exists in nova service list".format(service)) 
+            check_for = {'6':'compute1','8':'nova','10':'enabled','12':'up'}
+            
+            for location, correct_value in check_for.iteritems():
+                if (run("nova service-list | awk '/{}/ {print $"+ location +"}'".format(service)) == correct_value):
+                print align_y("{} host is {}".format(service, correct_value)) 
+            else:
+                print align_n("{} host is NOT {}".format(service, correct_value))  
+        else:
+            print align_n("{} does NOT exist in nova service list".format(service)) 
+
+                
+                '''        
+                if (run("nova service-list | awk '/{}/ {print $6}'".format(service)) == 'controller'):
+                    print align_y("{} host is controller".format(service)) 
+                else:
+                    print align_n("{} host is NOT controller".format(service)) 
+
+                if (run("nova service-list | awk '/{}/ {print $8}'".format(service)) == 'internal'):
+                    print align_y("{} host is internal".format(service)) 
+                else:
+                    print align_n("{} host is NOT internal".format(service)) 
+
+                if (run("nova service-list | awk '/{}/ {print $10}'".format(service)) == 'enabled'):
+                    print align_y("{} host is enabled".format(service)) 
+                else:
+                    print align_n("{} host is NOT enabled".format(service)) 
+
+                if (run("nova service-list | awk '/{}/ {print $12}'".format(service)) == 'up'):
+                    print align_y("{} host is controller".format(service)) 
+                else:
+                    print align_n("{} host is NOT controller".format(service)) 
+                '''
+
+
+        # NEED TO DO TDD FOR THE ARGUMENT BELOW
+        run("nova image-list")
     
 
 def tdd():
