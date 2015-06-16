4e8d7c8fb2ea88d83f9d53d070ed005285e043c9
Vitor <vitor.vfm@gmail.com>
2015-06-16 15:09:48 -0600

    Set up Gluster on Nova

--- env_config.py
+++ env_config.py
@@ -235,6 +235,7 @@ else:
     ##############################################################################
 
     glanceGlusterBrick = '/mnt/gluster/glance/images'
+    novaGlusterBrick = '/mnt/gluster/instance'
 
 
 
