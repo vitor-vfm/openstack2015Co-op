26f582b3769a8486bcaf29bb8e0fade3f453682f
Umair Hussain <suhussai@ualberta.ca>
2015-06-18 15:38:32 -0600

    defined function to setup keystone ceilometer

--- 11-ceilometer_deployment/fabfile.py
+++ 11-ceilometer_deployment/fabfile.py
@@ -22,6 +22,46 @@ ceilometer_config_file = "/etc/ceilometer/ceilometer.conf"
 
 
 ######################## Deployment ########################################
+
+def setup_ceilometer_keystone(CEILOMETER_PASS):
+    """
+    Set up Keystone credentials for Ceilometer
+
+    Create (a) a user and a service called 'ceilometer', and 
+    (b) an endpoint for the 'ceilometer' service
+    """
+
+    # get admin credentials to run the CLI commands
+    credentials = env_config.admin_openrc
+
+    with prefix(credentials):
+        # before each creation, we check a list to avoid duplicates
+
+        if 'ceilometer' not in run("keystone user-list"):
+            msg = "Create user ceilometer"
+            runCheck(msg, "keystone user-create --name ceilometer --pass {}".format(CEILOMETER_PASS))
+
+            msg = "Give the user 'ceilometer the role of admin"
+            runCheck(msg, "keystone user-role-add --user ceilometer --tenant service --role admin")
+        else:
+            print blue("User ceilometer already created. Do nothing")
+
+        if 'ceilometer' not in run("keystone service-list"):
+            msg = "Create service ceilometer"
+            runCheck(msg, "keystone service-create --name ceilometer --type image --description 'Telemetry'")
+        else:
+            print blue("Service ceilometer already created. Do nothing")
+
+        if 'http://controller:9292' not in run("keystone endpoint-list"):
+            msg = "Create endpoint for service ceilometer"
+            runCheck(msg, "keystone endpoint-create " + \
+                    "--service-id $(keystone service-list | awk '/ metering / {print $2}') " +\
+                    "--publicurl http://controller:8777 " + \
+                    "--internalurl http://controller:8777 " + \
+                    "--adminurl http://controller:8777 " + \
+                    "--region regionOne")
+        else:
+            print blue("Endpoint for service ceilometer already created. Do nothing")
    
 @roles('controller')
 def setup_ceilometer_controller():
