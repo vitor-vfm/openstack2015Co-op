fae19129c2968af545cf873ca0e62bc5a881406d
Derek <shultz.derek@gmail.com>
2015-06-19 15:29:56 -0600

    Working keystone fabfile

--- env_config.py
+++ env_config.py
@@ -5,11 +5,35 @@ from fabric.api import run, sudo, env
 from fabric.colors import red, green
 from fabric.api import *
 
-######################### Global variables ######################
+######################### Global variables ####################################
 
-lslogs = ['/var/log/nova/nova-manage.log', '/var/log/nova/nova-api.log', '/var/log/heat/heat-manage.log', '/var/log/glance/api.log', '/var/log/nova/nova-novncproxy.log', '/var/log/nova/nova-consoleauth.log', '/var/log/nova/nova-api.log', '/var/log/keystone/keystone.log', '/var/log/heat/heat-api-cfn.log', '/var/log/heat/heat-api.log', '/var/log/neutron/server.log', '/var/log/nova/nova-conductor.log', '/var/log/heat/heat-manage.log', '/var/log/nova/nova-scheduler.log', '/var/log/rabbitmq/rabbit@localhost.log', '/tmp/test.log', '/var/log/heat/heat-engine.log', '/var/log/mariadb/server.log', '/var/log/rabbitmq/rabbit@localhost-sasl.log', '/var/log/nova/nova-cert.log', '/var/log/rabbitmq/rabbit@localhost.log', '/var/log/keystone/keystone-tokenflush.log', '/var/log/glance/registry.log','/var/log/glusterfs/etc-glusterfs-glusterd.vol.log']
+lslogs = ['/var/log/nova/nova-manage.log',
+          '/var/log/nova/nova-api.log',
+          '/var/log/heat/heat-manage.log',
+          '/var/log/glance/api.log',
+          '/var/log/nova/nova-novncproxy.log',
+          '/var/log/nova/nova-consoleauth.log',
+          '/var/log/nova/nova-api.log',
+          '/var/log/keystone/keystone.log',
+          '/var/log/heat/heat-api-cfn.log',
+          '/var/log/heat/heat-api.log',
+          '/var/log/neutron/server.log',
+          '/var/log/nova/nova-conductor.log',
+          '/var/log/heat/heat-manage.log',
+          '/var/log/nova/nova-scheduler.log',
+          '/var/log/rabbitmq/rabbit@localhost.log',
+          '/tmp/test.log',
+          '/var/log/heat/heat-engine.log',
+          '/var/log/mariadb/server.log',
+          '/var/log/rabbitmq/rabbit@localhost-sasl.log',
+          '/var/log/nova/nova-cert.log',
+          '/var/log/rabbitmq/rabbit@localhost.log',
+          '/var/log/keystone/keystone-tokenflush.log',
+          '/var/log/glance/registry.log',
+          '/var/log/glusterfs/etc-glusterfs-glusterd.vol.log',
+          ]
 
-##############################################################################
+###############################################################################
 
 
 ########  ########   #######  ########  
@@ -21,14 +45,19 @@ lslogs = ['/var/log/nova/nova-manage.log', '/var/log/nova/nova-api.log', '/var/l
 ##        ##     ##  #######  ########  
 
 
-##############################################################################
+###############################################################################
 
 if 'ipmi5' in check_output('echo $HOSTNAME',shell=True):
 	# PRODUCTION
-	roledefs = { 'compute' : ['root@compute1','root@compute2','root@compute3','root@compute4' ],
-                 'network' : ['root@network'],
-                 'storage' : ['root@storage'],
-                 'controller' : ['root@controller']}
+	roledefs = { 'compute' : ['root@compute1',
+                                  'root@compute2',
+                                  'root@compute3',
+                                  'root@compute4',
+                                  ],
+                     'network' : ['root@network'],
+                     'storage' : ['root@storage'],
+                     'controller' : ['root@controller']}
+
 	logfilename='/opt/coop2015/coop2015/fabric.log'
 
 	my_cnf="""
@@ -59,26 +88,26 @@ pid-file=/var/run/mariadb/mariadb.pid
 """
     # passwords
 	passwd = { 'METADATA_SECRET' : '34m3t$3c43',
-               'ROOT_SECRET' : '34root43',
-               'RABBIT_PASS' : '34RabbGuest43',
-               'NOVA_DBPASS' : '34nova_db43',
-               'NEUTRON_DBPASS' : '34neu43',
-               'HEAT_DBPASS' : '34heat_db43',
-               'GLANCE_DBPASS' : '34glance_db43',
-               'SAHARA_DBPASS' : '34sahara_db43',
-               'CINDER_DBPASS' : '34cinder_db43',
-               'ADMIN_PASS' : '34adm43',
-               'DEMO_PASS' : '34demo43',
-               'KEYSTONE_DBPASS' : '34keydb43',
-               'NOVA_PASS' : '34nova_ks43',
-               'NEUTRON_PASS' : '34neu43',
-               'HEAT_PASS' : '34heat_ks43',
-               'GLANCE_PASS' : '34glance_ks43',
-               'SAHARA_PASS' : '34sahara_ks43',
-               'CINDER_PASS' : '34cinder_ks43',
-               'SWIFT_PASS' : '34$w1f43',
-               'TROVE_PASS' : '34Tr0v343',
-               'TROVE_DBPASS' : '34Tr0v3db4s343'}
+                   'ROOT_SECRET' : '34root43',
+                   'RABBIT_PASS' : '34RabbGuest43',
+                   'NOVA_DBPASS' : '34nova_db43',
+                   'NEUTRON_DBPASS' : '34neu43',
+                   'HEAT_DBPASS' : '34heat_db43',
+                   'GLANCE_DBPASS' : '34glance_db43',
+                   'SAHARA_DBPASS' : '34sahara_db43',
+                   'CINDER_DBPASS' : '34cinder_db43',
+                   'ADMIN_PASS' : '34adm43',
+                   'DEMO_PASS' : '34demo43',
+                   'KEYSTONE_DBPASS' : '34keydb43',
+                   'NOVA_PASS' : '34nova_ks43',
+                   'NEUTRON_PASS' : '34neu43',
+                   'HEAT_PASS' : '34heat_ks43',
+                   'GLANCE_PASS' : '34glance_ks43',
+                   'SAHARA_PASS' : '34sahara_ks43',
+                   'CINDER_PASS' : '34cinder_ks43',
+                   'SWIFT_PASS' : '34$w1f43',
+                   'TROVE_PASS' : '34Tr0v343',
+                   'TROVE_DBPASS' : '34Tr0v3db4s343'}
 
 
 ##############################################################################
@@ -101,42 +130,45 @@ else:
 
 
     my_cnf="""
-[mysqld]
-datadir=/var/lib/mysql
-socket=/var/lib/mysql/mysql.sock
-# Disabling symbolic-links is recommended to prevent assorted security risks
-symbolic-links=0
-# Settings user and group are ignored when systemd is used.
-# If you need to run mysqld under a different user or group,
-# customize your systemd unit file for mariadb according to the
-# instructions in http://fedoraproject.org/wiki/Systemd
-bind-address = 192.168.1.11
-default-storage-engine = innodb
-innodb_file_per_table
-collation-server = utf8_general_ci
-init-connect = 'SET NAMES utf8'
-character-set-server = utf8
+           [mysqld]
+           datadir=/var/lib/mysql
+           socket=/var/lib/mysql/mysql.sock
+           # Disabling symbolic-links is recommended to prevent assorted security risks
+           symbolic-links=0
+           # Settings user and group are ignored when systemd is used.
+           # If you need to run mysqld under a different user or group,
+           # customize your systemd unit file for mariadb according to the
+           # instructions in http://fedoraproject.org/wiki/Systemd
+           bind-address = BIND_ADDRESS
+           default-storage-engine = innodb
+           innodb_file_per_table
+           collation-server = utf8_general_ci
+           init-connect = 'SET NAMES utf8'
+           character-set-server = utf8
 
-[mysqld_safe]
-log-error=/var/log/mariadb/mariadb.log
-pid-file=/var/run/mariadb/mariadb.pid
+           [mysqld_safe]
+           log-error=/var/log/mariadb/mariadb.log
+           pid-file=/var/run/mariadb/mariadb.pid
 
-#
-# include all files from the config directory
-#
-!includedir /etc/my.cnf.d
-"""
+           #
+           # include all files from the config directory
+           #
+           !includedir /etc/my.cnf.d
+           """
     # for the env dictionary
-    roledefs = { 'compute' : ['root@computeVM'],
-                 'network' : ['root@networkVM'],
-                 'storage' : ['root@storageVM'],
-                 'controller' : ['root@controllerVM']}
+    roledefs = { 'compute' : ['root@compute1'],
+                 'network' : ['root@network'],
+                 'storage' : ['root@storage1'],
+                 'controller' : ['root@controller']}
 
     roles = roledefs.keys()
     hosts = roledefs.values()
 
     # ntp
-    ntpServers = ['time1.srv.ualberta.ca','time2.srv.ualberta.ca','time3.srv.ualberta.ca']
+    ntpServers = ['time1.srv.ualberta.ca',
+                  'time2.srv.ualberta.ca',
+                  'time3.srv.ualberta.ca',
+                  ]
 
     # passwords
     passwd = { 'METADATA_SECRET' : '34m3t$3c43',
@@ -159,9 +191,12 @@ pid-file=/var/run/mariadb/mariadb.pid
                'CINDER_PASS' : '34cinder_ks43',
                'SWIFT_PASS' : '34$w1f43',
                'TROVE_PASS' : '34Tr0v343',
-               'TROVE_DBPASS' : '34Tr0v3db4s343'}
+               'TROVE_DBPASS' : '34Tr0v3db4s343',
+               'CEILOMETER_DBPASS' : '34ceilometer_db43',
+               'CEILOMETER_PASS' : '34ceilometer_ks43',
+               }
 
-    ##############################################################################
+    ###########################################################################
 
     ##    ## ######## ######## ##      ##  #######  ########  ##    ## 
     ###   ## ##          ##    ##  ##  ## ##     ## ##     ## ##   ##  
@@ -171,41 +206,64 @@ pid-file=/var/run/mariadb/mariadb.pid
     ##   ### ##          ##    ##  ##  ## ##     ## ##    ##  ##   ##  
     ##    ## ########    ##     ###  ###   #######  ##     ## ##    ## 
 
-    ##############################################################################
+    ###########################################################################
   
-    controllerManagement = { 'DEVICE' : 'eth1',
+    controllerManagement = { 'DEVICE' : 'eno1',
                              'IPADDR' : '192.168.1.11',
-                             'NETMASK' : '255.255.255.0'}
+                             'NETMASK' : '255.255.255.0',
+                             'GATEWAY' : '192.168.1.1',
+                             'DNS1' : '129.128.208.13',
+                             }
 
-    controllerTunnels = { 'DEVICE' : 'eth2',
+    controllerTunnels = { 'DEVICE' : 'enp2s10',
                           'IPADDR' : '192.168.2.11',
-                          'NETMASK' : '255.255.255.0'}
+                          'GATEWAY' : '192.168.1.1',
+                          'DNS1' : '129.128.208.13',
+                          'NETMASK' : '255.255.255.0',
+                          }
 
-    networkManagement = { 'DEVICE' : 'eth1',
+    networkManagement = { 'DEVICE' : 'eno1',
                           'IPADDR' : '192.168.1.21',
-                          'NETMASK' : '255.255.255.0'}
+                          'GATEWAY' : '192.168.1.1',
+                          'DNS1' : '129.128.208.13',
+                          'NETMASK' : '255.255.255.0',
+                          }
 
-    networkTunnels = { 'DEVICE' : 'eth2',
+    networkTunnels = { 'DEVICE' : 'enp2s10',
                        'IPADDR' : '192.168.2.21',
-                       'NETMASK' : '255.255.255.0'}
+                       'GATEWAY' : '192.168.1.1',
+                       'DNS1' : '129.128.208.13',
+                       'NETMASK' : '255.255.255.0',
+                       }
 
-    networkExternal = { 'DEVICE' : 'eth3',
+    networkExternal = { 'DEVICE' : 'enp2s12',
                         'TYPE' : 'Ethernet',
                         'ONBOOT' : '"yes"',
                         'BOOTPROTO' : '"none"',
+                        'GATEWAY' : '192.168.1.1',
+                        'DNS1' : '129.128.208.13',
                         'IPADDR' : '192.168.3.21'}
 
-    computeManagement = { 'DEVICE' : 'eth1',
+    computeManagement = { 'DEVICE' : 'eno1',
                           'IPADDR' : '192.168.1.41',
-                          'NETMASK' : '255.255.255.0'}
+                          'GATEWAY' : '192.168.1.1',
+                          'DNS1' : '129.128.208.13',
+                          'NETMASK' : '255.255.255.0',
+                          }
 
-    computeTunnels = { 'DEVICE' : 'eth2',
+    computeTunnels = { 'DEVICE' : 'enp2s10',
+                       'GATEWAY' : '192.168.1.1',
+                       'DNS1' : '129.128.208.13',
                        'IPADDR' : '192.168.2.41',
-                       'NETMASK' : '255.255.255.0'}
+                       'NETMASK' : '255.255.255.0',
+                       }
 
-    storageManagement = {   'DEVICE' : 'eth1',
+    storageManagement = {   'DEVICE' : 'enp0s25',
                             'IPADDR' : '192.168.1.31',
-                            'NETMASK' : '255.255.255.0'}
+                            'GATEWAY' : '192.168.1.1',
+                            'DNS1' : '129.128.208.13',
+                            'NETMASK' : '255.255.255.0',
+                            }
 
     hosts = { controllerManagement['IPADDR'] : 'controller',
               networkManagement['IPADDR'] : 'network'}
@@ -233,7 +291,7 @@ pid-file=/var/run/mariadb/mariadb.pid
         hosts[IP] = 'storage' + str(i+1)
 
 
-    ##############################################################################
+    ###########################################################################
 
     ##    ## ######## ##    ##  ######  ########  #######  ##    ## ######## 
     ##   ##  ##        ##  ##  ##    ##    ##    ##     ## ###   ## ##       
@@ -243,7 +301,7 @@ pid-file=/var/run/mariadb/mariadb.pid
     ##   ##  ##          ##    ##    ##    ##    ##     ## ##   ### ##       
     ##    ## ########    ##     ######     ##     #######  ##    ## ######## 
 
-    ##############################################################################
+    ###########################################################################
 
 
     keystone_emails = { 'ADMIN_EMAIL' : 'admin@example.com',
@@ -251,7 +309,7 @@ pid-file=/var/run/mariadb/mariadb.pid
 
 
 
-    ##############################################################################
+    ###########################################################################
 
     ##    ## ######## ##     ## ######## ########   #######  ##    ## 
     ###   ## ##       ##     ##    ##    ##     ## ##     ## ###   ## 
@@ -260,12 +318,12 @@ pid-file=/var/run/mariadb/mariadb.pid
     ##  #### ##       ##     ##    ##    ##   ##   ##     ## ##  #### 
     ##   ### ##       ##     ##    ##    ##    ##  ##     ## ##   ### 
     ##    ## ########  #######     ##    ##     ##  #######  ##    ## 
-    ##############################################################################
+    ############################################################################
 
 
 
 
-    ##############################################################################
+    ###########################################################################
 
      ######  ########  #######  ########     ###     ######   ######## 
     ##    ##    ##    ##     ## ##     ##   ## ##   ##    ##  ##       
@@ -275,7 +333,7 @@ pid-file=/var/run/mariadb/mariadb.pid
     ##    ##    ##    ##     ## ##    ##  ##     ## ##    ##  ##       
      ######     ##     #######  ##     ## ##     ##  ######   ######## 
 
-    ##############################################################################
+    ###########################################################################
 
     partition = {   'size_reduction_of_home' : '3.5G',
                     'partition_size' : '500M' }
