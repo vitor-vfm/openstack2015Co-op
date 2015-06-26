5af8cba17639ce5428a89a48d75ed0a44f3f5159
Umair Hussain <suhussai@ualberta.ca>
2015-06-26 12:48:55 -0600

    port checking added

--- port_check.sh
+++ port_check.sh
@@ -18,48 +18,53 @@ EOF
 
 component=$1
 
-function check_port {
+case "$1" in 
+    0)
+	ports=""
+	;;
+    1)
+	ports=""
+	;;
+    2)
+	ports="5672"
+	;;
+    3)
+	ports="35357 5000"
+	;;
+    4)
+	ports="9292"
+	;;
+    5)
+	ports="8774"
+	;;
+    6)
+	ports="9696"
+	;;
+    7)
+	ports=""
+	;;
+    8)
+	ports="8776"
+	;;
+    9)
+	ports="8080"
+	;;
+    10)
+	ports="8004"
+	;;
+    11)
+	ports="8777"
+
+	;;
+esac
+
+nodes="controller compute1 network storage1"
+for port in $ports;
+do
+    for node in $nodes;
+    do
+	ssh root@$node "ss -nltp | grep $port"
+    done
+
+done
     
-    case "$1" in 
-	0)
-	    ports=""
-	    ;;
-	1)
-	    ports=""
-	    ;;
-	2)
-	    ports="5672"
-	    ;;
-	3)
-	    ports="35357 5000"
-	    ;;
-	4)
-	    ports="9292"
-	    ;;
-	5)
-	    ports="8774"
-	    ;;
-	6)
-	    ports="9696"
-	    ;;
-	7)
-	    ports=""
-	    ;;
-	8)
-	    ports="8776"
-	    ;;
-	9)
-	    ports="8080"
-	    ;;
-	10)
-	    ports="8004"
-	    ;;
-	11)
-	    ports="8777"
-
-	    ;;
-    esac
-
-
-
-}
