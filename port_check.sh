15dd42f225d2c45aadc2b18156b9fe0d0242fb65
Umair Hussain <suhussai@ualberta.ca>
2015-06-26 12:58:37 -0600

    finished first version of script.

--- port_check.sh
+++ port_check.sh
@@ -16,55 +16,90 @@ To check for listeners for component number 2
 
 EOF
 
-component=$1
+start=$1
+end=$2
 
-case "$1" in 
-    0)
-	ports=""
-	;;
-    1)
-	ports=""
-	;;
-    2)
-	ports="5672"
-	;;
-    3)
-	ports="35357 5000"
-	;;
-    4)
-	ports="9292"
-	;;
-    5)
-	ports="8774"
-	;;
-    6)
-	ports="9696"
-	;;
-    7)
-	ports=""
-	;;
-    8)
-	ports="8776"
-	;;
-    9)
-	ports="8080"
-	;;
-    10)
-	ports="8004"
-	;;
-    11)
-	ports="8777"
+function check_port {
+    nodes="controller compute1 network storage1"
+    case "$1" in 
+	0)
+	    ports=""
+	    ;;
+	1)
+	    ports=""
+	    ;;
+	2)
+	    ports="5672"
+	    ;;
+	3)
+	    ports="35357 5000"
+	    ;;
+	4)
+	    ports="9292"
+	    ;;
+	5)
+	    ports="8774"
+	    ;;
+	6)
+	    ports="9696"
+	    ;;
+	7)
+	    ports=""
+	    ;;
+	8)
+	    ports="8776"
+	    ;;
+	9)
+	    ports="8080"
+	    ;;
+	10)
+	    ports="8004"
+	    ;;
+	11)
+	    ports="8777"
 
-	;;
-esac
+	    ;;
+    esac
 
-nodes="controller compute1 network storage1"
-for port in $ports;
-do
-    for node in $nodes;
-    do
-	ssh root@$node "ss -nltp | grep $port"
-    done
 
-done
+    red=`tput setaf 1`
+    green=`tput setaf 2`
+    reset=`tput sgr0`
     
+    for port in $ports;
+    do
+
+	echo -e '\n\n'
+	echo '##################'
+	echo 'Checking port ' $port ' in component ' $1
+	echo '##################'
+	for node in $nodes;
+	do
+	    output=$(ssh root@$node "ss -nltp | grep $port")
+	    if [[ "$output" =~ ^LISTEN ]]
+	    then
+		echo "${green}LISTENER on $node ${reset}"	    
+		
+	    else
+		echo "${red}No LISTENER on $node ${reset}"	    
+		
+	    fi
+	done
+
+    done
+}    
+
+
+if ! [ -z "$end"  ]
+then
+    for i in $(seq $start $end); 
+    do
+	check_port $i
+	
+    done
+else
+    # to handle the case when a single 
+    # digit is specified in the range
+    check_port $start
+    
+fi
