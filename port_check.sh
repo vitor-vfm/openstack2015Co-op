d99d78cdc9fb08318ce5e1a40eef871ef2980543
Umair Hussain <suhussai@ualberta.ca>
2015-06-26 12:42:53 -0600

    listed port numbers

---
+++ port_check.sh
@@ -0,0 +1,65 @@
+#! /bin/bash
+
+<<EOF
+
+Checks whether or not a port listener is setup for a given 
+openstack component using ss -nltp | grep port 
+
+Requires:
+
+component
+
+usage:
+
+To check for listeners for component number 2
+./port_check 2
+
+EOF
+
+component=$1
+
+function check_port {
+    
+    case "$1" in 
+	0)
+	    ports=""
+	    ;;
+	1)
+	    ports=""
+	    ;;
+	2)
+	    ports="5672"
+	    ;;
+	3)
+	    ports="35357 5000"
+	    ;;
+	4)
+	    ports="9292"
+	    ;;
+	5)
+	    ports="8774"
+	    ;;
+	6)
+	    ports="9696"
+	    ;;
+	7)
+	    ports=""
+	    ;;
+	8)
+	    ports="8776"
+	    ;;
+	9)
+	    ports="8080"
+	    ;;
+	10)
+	    ports="8004"
+	    ;;
+	11)
+	    ports="8777"
+
+	    ;;
+    esac
+
+
+
+}
