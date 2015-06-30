a9c36d4d6410a11e8ed8bb7fce8a6e1f6c6e58c4
Umair Hussain <suhussai@ualberta.ca>
2015-06-30 10:19:56 -0600

    fixed merge errors

--- port_check.sh
+++ port_check.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+<<<<<<< HEAD:port_check.sh
 <<EOF
 
 Checks whether or not a port listener is setup for a given 
@@ -15,11 +16,67 @@ To check for listeners for component number 2
 ./port_check 2
 
 EOF
+=======
 
-start=$1
-end=$2
+# Checks whether or not a port listener is setup for a given 
+# openstack component using ss -nltp | grep port 
+#
+# Requires:
+#
+# component - can be specified in a range
+#
+# usage:
+#
+# To check for listeners for component number 2 use -s(tart)
+# bash port_check -s 2
+#
+# To check for listeners for components from 2 to 7 -s(tart) and -e(nd)
+# bash port_check -s 2 -e 7
+#
+#
+#
+# Verbose options:
+# -v ---> shows which component name is found on what node(s) 
+#         and the port associated with it
+#
+# -vv ---> shows which component name is found on what node(s) 
+#          and the port associated as well as the output of the 
+# 	 ss -ntlp | grep port command
 
+
+
+# get component dictionary variable in order 
+# to be able to replace component numbers with
+# component names
 source dictionary.sh
+
+OPTIND=1
+verbosity=0
+
+while getopts "vs:e:" OPTION
+do
+
+    case $OPTION in 
+	v)
+	    verbosity=$(($verbosity+1))
+	    ;;
+	s)
+	    start=$OPTARG
+	    ;;
+
+	e)
+	    end=$OPTARG
+	    ;;
+	esac
+done
+
+# shift argument so that verbose options don't conflict with arguments 
+# for script 
+shift $((OPTIND-1))
+
+#start=$1
+#end=$2
+
 
 function check_port {
     nodes="controller compute1 network storage1"
@@ -71,18 +128,31 @@ function check_port {
     for port in $ports;
     do
 
-	echo -e '\n\n'
-	echo '#########################################'
-	echo 'Checking port ' $port ' in component ' ${component_dictionary[$1]}
-	echo '#########################################'
+	
+	if [ $verbosity -gt 1 ]
+	then
+	    echo -e '\n'
+	    echo '##############################################################################'
+	    echo 'Checking port ' $port ' in component ' ${component_dictionary[$1]}
+	    echo '##############################################################################'
+	fi
+
 	for node in $nodes;
 	do
 	    output=$(ssh root@$node "ss -nltp | grep $port")
 	    if [[ "$output" =~ ^LISTEN ]]
 	    then
-		echo "${green}LISTENER found on $node ${reset}: "$output
+		if [ $verbosity == 0 ]
+		then
+		    echo "${green}${component_dictionary[$1]} listener found on $node ${reset}"		    
+		elif [ $verbosity == 1 ]
+		then
+		    echo "${green}${component_dictionary[$1]} listener port $port found on $node ${reset}"
+		else
+		    echo "${green}${component_dictionary[$1]} listener on port $port found on $node ${reset}: "$output		    
+		fi
 	    else
-		echo "${red}No LISTENER found on $node ${reset}"	    
+	        echo "${red}${component_dictionary[$1]} listener NOT found on $node ${reset}"	    
 		
 	    fi
 	done
@@ -90,17 +160,20 @@ function check_port {
     done
 }    
 
-
-if ! [ -z "$end"  ]
+if ! [ -z "$end"  ] && ! [ -z "$start" ]
 then
     for i in $(seq $start $end); 
     do
 	check_port $i
 	
     done
-else
+elif ! [ -z "$start" ]
+then
     # to handle the case when a single 
     # digit is specified in the range
     check_port $start
+
+else 
+    echo "--Arguments for start and end missing"
     
 fi
