254687f7a08691e52741f1c8afddf9b528478cd6
Vitor <vitor.vfm@gmail.com>
2015-07-07 14:29:46 -0600

    Removed all storage node setup (Network)

--- 1-network_deployment/fabfile.py
+++ 1-network_deployment/fabfile.py
@@ -31,9 +31,11 @@ def debug_str(command):
     return blue(sudo(command,quiet=True))
 
 ########################## Deployment ########################################
-@roles('controller','compute','network', 'storage')
+
+@roles('controller','compute','network')
 def deployNIC():
     config_file = ''
+    print env.host
     if (nicDictionary[env.host]['tnlDEVICE']!=''):
         config_file += "DEVICE=" +nicDictionary[env.host]['tnlDEVICE'] + '\n'
         config_file += "IPADDR=" +nicDictionary[env.host]['tnlIPADDR'] + '\n'
@@ -49,7 +51,7 @@ def deploy():
 
 ################################ TDD #########################################
 
-@roles('controller','compute','network', 'storage')
+@roles('controller','compute','network')
 def tdd():
 
     pings = [
