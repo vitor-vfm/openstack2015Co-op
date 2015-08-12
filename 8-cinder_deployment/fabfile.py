d7aa4cfe9418211fc5aefaf30ffa7e31edf40641
Vitor <vitor.vfm@gmail.com>
2015-08-12 13:32:07 -0600

    NFS installation in storage nodes checks if NFS is already installed (Cinder)

--- 8-cinder_deployment/fabfile.py
+++ 8-cinder_deployment/fabfile.py
@@ -227,11 +227,9 @@ def restart_cinder():
 
 ############################## NFS ############################################
 
-@roles('storage')
 def install_nfs_on_storage():
     runCheck("Install NFS", "yum install nfs-utils rpcbind -y")
 
-@roles('storage')
 def make_nfs_directories():
     runCheck("Make nfs cinder directory", "mkdir %s" % nfs_share)
     runCheck("Make nfs swift directory", "mkdir /home/swift")
@@ -239,14 +237,24 @@ def make_nfs_directories():
     runCheck("Setup exports file", "echo '%s 192.168.1.0/24(rw,sync)'>/etc/exports" % nfs_share)
     runCheck("Continue setting up exports file", "echo '/home/swift 192.168.1.0/24(rw,sync)'>>/etc/exports")
     
-    runCheck("Change cinder NFS file permissions", "chown -R 65534:65534 %s/" nfs_share)
+    runCheck("Change cinder NFS file permissions", "chown -R 65534:65534 %s/" % nfs_share)
     
-@roles('storage')
 def export_and_start_nfs():
     runCheck("Export the file system", "exportfs -a")
     
     runCheck("Start rpcbind", "service rpcbind start && chkconfig rpcbind on")
     runCheck("Start NFS", "service rpcbind start; service nfs start")
+
+@roles('storage')
+def customize_storage_for_nfs():
+    # Check if it has already been deployed
+    if run("systemctl status nfs-server | grep 'Active: active'").return_code == 0:
+        print blue('NFS already deployed in %s. Nothing done' % env.host)
+        return
+
+    install_nfs_on_storage()
+    make_nfs_directories()
+    export_and_start_nfs()
 
 @roles('controller')
 def change_shares_file_for_nfs():
@@ -299,9 +307,7 @@ def deploy():
     #execute(restart_cinder)
 
     # customize storage node for nfs
-    execute(install_nfs_on_storage)
-    execute(make_nfs_directories)
-    execute(export_and_start_nfs)
+    execute(customize_storage_for_nfs)
     
     # customize cinder for nfs
     execute(install_nfs_on_controller)
